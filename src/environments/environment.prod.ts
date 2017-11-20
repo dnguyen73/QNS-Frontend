8258cda81eef3ae4ba7a95183876d6af95ada9b6
Duy Nguyen <dnguyen73@csc.com>
2017-11-20 10:46:46 +0700

    change loopback endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
 
   //URL of development API
-  apiUrl: 'http://139.59.102.174/:3000/api',
+  apiUrl: 'http://139.59.102.174:3000/api',
 
   //Domain address
-  FILE_HOST_URL: 'http://139.59.102.174/:3000/files'
+  FILE_HOST_URL: 'http://139.59.102.174:3000/files'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,10 +8,10 @@ export const environment = {
 
   //URL of development API
   //apiUrl: 'http://localhost:3000/api',
-  apiUrl: 'http://139.59.102.174/:3000/api',
+  apiUrl: 'http://139.59.102.174:3000/api',
 
   //Domain address
   //FILE_HOST_URL: 'http://localhost:3000/files',
-  FILE_HOST_URL: 'http://139.59.102.174/:3000/files'
+  FILE_HOST_URL: 'http://139.59.102.174:3000/files'
 };
 
