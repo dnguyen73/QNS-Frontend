34e284b699781a39311237c159fbb0638a792065
Duy Nguyen <dnguyen73@csc.com>
2017-11-29 04:30:40 +0700

    fix currency format on diff region setting

--- src/app/shared/pipes/qns-price.pipe.ts
+++ src/app/shared/pipes/qns-price.pipe.ts
@@ -1,13 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
 
 @Pipe({name: 'vnd'})
 export class QnsPricePipe implements PipeTransform {
   transform(value: string, args: string[]): any {
     if (!value) return value;
 
-    var currencyPipe = new CurrencyPipe("vi-VN");
-    value = currencyPipe.transform(value, "VND", true);
-    return value.substr(2) + ' đ';
+    var currencyPipe = new DecimalPipe("vi-VN");
+    return currencyPipe.transform(value) + ' đ';
   }
 }
